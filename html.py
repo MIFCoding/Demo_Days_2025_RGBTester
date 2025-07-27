b0787795761619bb7d120b0aa13f83c89cd1f6b7
MIFCoding <mifprogramming@gmail.com>
2025-07-27 21:42:08 +0300

    Fix 3 path to reports

--- html.py
+++ html.py
@@ -399,7 +399,7 @@ def generate_html_report(results):
     """
     
     # Сохранение отчета
-    with open(f"reports/{filename}", "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(html_content)
     
     print(f"\n{COLOR_GREEN}Отчёт сохранён как: {os.path.abspath(filename)}{COLOR_RESET}")
